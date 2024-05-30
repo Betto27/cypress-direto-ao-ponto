f58a38f1cd29c1870b4e36493501ce3c081c3193
betto <betto_beto@hotmail.com>
2024-05-30 17:45:30 -0300

    Primeiro commit direto ao ponto

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  projectId: "67koxy",
+  // ...rest of the Cypress project config
+};
